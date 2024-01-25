e8e7e36d7d8c2b671f8e361f9f506336adf42cf4
Avishka <78788710+avishka777dilshan@users.noreply.github.com>
2024-01-25 20:54:28 +0530

    Add files via upload

---
+++ cargame.py
@@ -0,0 +1,9 @@
+help_word = input(">")
+start_word = input()
+while help_word == "help" or help_word == "HELP":
+    print("start - to start the car")
+    print("stop - to stop the car")
+    print("quit - to exit")
+    break
+
+if
