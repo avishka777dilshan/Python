86cc7cc0515e173a4d2e8fce01c2d4547e3948e1
Avishka <78788710+avishka777dilshan@users.noreply.github.com>
2024-01-29 20:37:58 +0530

    Add files via upload

---
+++ comparison_operators_ex.py
@@ -0,0 +1,8 @@
+name = "Avishka"
+
+if len(name) < 3:
+    print("Name should have at least 3 characters")
+elif len(name) > 50:
+    print("Name should less than 50 characters")
+else:
+    print("good name")
