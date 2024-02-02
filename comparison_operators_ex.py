355c587e88ade04b4bfdc408760b2c5ee96d61ab
Avishka <78788710+avishka777dilshan@users.noreply.github.com>
2024-02-02 07:30:56 +0530

    Delete comparison_operators_ex.py

--- comparison_operators_ex.py
+++
@@ -1,8 +0,0 @@
-name = "Avishka"
-
-if len(name) < 3:
-    print("Name should have at least 3 characters")
-elif len(name) > 50:
-    print("Name should less than 50 characters")
-else:
-    print("good name")
